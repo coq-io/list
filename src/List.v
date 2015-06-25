34196b15b7104c33fb0f1995dd3c3ea0426afe71
Guillaume Claret <dev@clarus.me>
2015-06-25 12:13:39 +0200

    Parallel map added

--- src/List.v
+++ src/List.v
@@ -14,6 +14,16 @@ Fixpoint map_seq {E : Effect.t} {A B : Type} (f : A -> C.t E B) (l : list A)
     ret (y :: l)
   end.
 
+Fixpoint map_par {E : Effect.t} {A B : Type} (f : A -> C.t E B) (l : list A)
+  : C.t E (list B) :=
+  match l with
+  | [] => ret []
+  | x :: l =>
+    let! y_l := join (f x) (map_par f l) in
+    let (y, l) := y_l in
+    ret (y :: l)
+  end.
+
 Fixpoint iter_seq {E : Effect.t} {A : Type} (f : A -> C.t E unit) (l : list A)
   : C.t E unit :=
   match l with
@@ -51,5 +61,22 @@ Module Run.
     : Run.t (List.map_seq f (List.map x l)) l.
     rewrite <- List.map_id.
     now apply map_seq.
+  Defined.
+
+  Fixpoint map_par {E : Effect.t} {A B C : Type} {f : A -> C.t E B}
+    (l : list C) (x : C -> A) (y : C -> B)
+    (run_f : forall (v : C), Run.t (f (x v)) (y v)) {struct l}
+    : Run.t (map_par f (List.map x l)) (List.map y l).
+    destruct l as [|v l].
+    - apply Ret.
+    - eapply Let. apply (Join (run_f v) (map_par _ _ _ _ _ l x y run_f)).
+      apply Ret.
+  Defined.
+
+  Definition map_par_id {E : Effect.t} {A B : Type} {f : A -> C.t E B}
+    (l : list B) (x : B -> A) (run_f : forall (v : B), Run.t (f (x v)) v)
+    : Run.t (List.map_par f (List.map x l)) l.
+    rewrite <- List.map_id.
+    now apply map_par.
   Defined.
 End Run.
